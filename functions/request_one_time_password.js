c12dce445cfa64f082fee08dea80af27325abfff
Bryce Hull <bahull@live.com>
2019-02-16 22:30:01 -0600

    Update var

--- functions/request_one_time_password.js
+++ functions/request_one_time_password.js
@@ -11,29 +11,25 @@ module.exports = function(req, res) {
   admin
     .auth()
     .getUser(phone)
-    .then(
-      userRecord => {
-        const code = Math.floor(Math.random() * 8999 + 1000);
+    .then(userRecord => {
+      const code = Math.floor(Math.random() * 8999 + 1000);
 
-        twilio.messages.create({
-          body: `Your code is ${code}`,
-          to: phone,
-          from: "+15012224246"
+      twilio.messages.create({
+        body: `Your code is ${code}`,
+        to: phone,
+        from: "+15012224246"
+      });
+    })
+    .then(resp => {
+      console.log("I amde it");
+      console.log("in the db");
+      admin
+        .database()
+        .ref("users/" + phone)
+        .update({ code: code, codeValid: true }, err => {
+          return console.log(err, "success on db");
         });
-      },
-      err => {
-        if (err) {
-          return res.status(422).send({ error: `${err}: IN THE TWILIO ` });
-        }
-
-        admin
-          .database()
-          .ref("users/" + phone)
-          .update({ code: code, codeValid: true }, () => {
-            res.send({ success: true });
-          });
-      }
-    )
+    })
     .catch(err => {
       return res.status(422).send({ error: `${err}: IN THE TWILIO ` });
     });
